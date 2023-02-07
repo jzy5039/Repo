bdb312a6d68055bbb7cdcd76d5a3f8a8c346375a
jonathanyulan99 <jyulancogollo@gmail.com>
2023-02-07 09:41:11 -0500

    Setting Up Testing React Environment

---
+++ Python/helloworld.py
@@ -0,0 +1,14 @@
+def helloworld() -> None:
+    print('Hello World!')
+
+
+def helloworld2() -> None:
+    print('Hello World <3')
+
+
+x = helloworld
+y = helloworld2
+
+list_1 = list([x, y])
+for function in list_1:
+    function()
